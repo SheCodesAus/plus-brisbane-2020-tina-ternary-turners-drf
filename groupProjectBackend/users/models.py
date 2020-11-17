791fe3c708bdcd6591412f9710660d0d7b68a335
RebeccaMillwood <bec-millwood@live.com.au>
2020-11-17 19:42:15 +1000

    added users models and methods and authentication

--- groupProjectBackend/users/models.py
+++ groupProjectBackend/users/models.py
@@ -1,1 +1,12 @@
-# testing git pull requests
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    first_name = models.TextField(max_length=30, blank=True)
+    last_name = models.TextField(max_length=30, blank=True)
+    email = models.TextField(max_length=30, blank=True)
+    password = models.CharField(max_length=30)
+   
+    
+    def __str__(self):
+        return self.username
---
+++ groupProjectBackend/users/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+class CustomUserSerializer(serializers.Serializer):
+    id = serializers.ReadOnlyField()
+    # username = serializers.CharField(max_length=200)
+    email = serializers.CharField(max_length=200)
+    password = serializers.CharField(write_only=True)
+    first_name = serializers.CharField(max_length=30)
+    last_name = serializers.CharField(max_length=30)
+
+    def create(self, validated_data):
+        return CustomUser.objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        # instance.username = validated_data.get('username', instance.username)
+        instance.email = validated_data.get('email', instance.email)
+        instance.password = validated_data.get('password', instance.password)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.save()
+        return instance
